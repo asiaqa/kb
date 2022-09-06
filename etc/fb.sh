c15ea9a4940e8b9a66055cbecd9a059bf08572ea
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-06 10:42:43 +0800

    Update fb.sh

--- etc/fb.sh
+++ etc/fb.sh
@@ -2,22 +2,22 @@
 
 # Configure filebrowser
 if [ ! -f "/fb/data/config/filebrowser.db" ]; then
-    filebrowser -d /fb/data/config/filebrowser.db config init
-    filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+    /fb/filebrowser -d /fb/data/config/filebrowser.db config init
+    /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME --locale zh-cn
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME --locale zh-cn
     fi
 else
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,t>
     else
-        filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     fi
-    filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME -p $FRP_PASS
+    /fb/filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME -p $FRP_PASS
 fi
 
-filebrowser -d /fb/data/config/filebrowser.db config set -r / -b $MYPATH/files -p 61801
+/fb/filebrowser -d /fb/data/config/filebrowser.db config set -r / -b $MYPATH/files -p 61801
 
 # Run filebrowser
 exec 2>&1
-exec filebrowser -d /fb/data/config/filebrowser.db
+exec /fb/filebrowser -d /fb/data/config/filebrowser.db
