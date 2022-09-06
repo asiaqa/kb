6e20f9f3df7d591ff76d600b22579734a89bafa5
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-06 10:26:38 +0800

    Update fb.sh

--- etc/fb.sh
+++ etc/fb.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # Configure filebrowser
-if [ ! -f "/mnt/data/config/filebrowser.db" ]; then
-    filebrowser -d /mnt/data/config/filebrowser.db config init
-    filebrowser -d /mnt/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+if [ ! -f "/fb/data/config/filebrowser.db" ]; then
+    filebrowser -d /fb/data/config/filebrowser.db config init
+    filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /mnt/data/config/filebrowser.db users update ${GLOBAL_USER} --locale zh-cn
+        filebrowser -d /fb/data/config/filebrowser.db users update ${GLOBAL_USER} --locale zh-cn
     fi
 else
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /mnt/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
     else
-        filebrowser -d /mnt/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     fi
-    filebrowser -d /mnt/data/config/filebrowser.db users update ${GLOBAL_USER} -p ${GLOBAL_PASSWORD}
+    filebrowser -d /fb/data/config/filebrowser.db users update ${GLOBAL_USER} -p ${GLOBAL_PASSWORD}
 fi
 
-filebrowser -d /mnt/data/config/filebrowser.db config set -r /mnt/data -b ${GLOBAL_PORTAL_PATH}/files -p 61801
+filebrowser -d /fb/data/config/filebrowser.db config set -r / -b ${GLOBAL_PORTAL_PATH}/files -p 61801
 
 # Run filebrowser
 exec 2>&1
-exec filebrowser -d /mnt/data/config/filebrowser.db
+exec filebrowser -d /fb/data/config/filebrowser.db
