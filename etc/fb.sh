73d45baee6574f4c69c690b0be4985369c6e80c2
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-06 10:32:39 +0800

    Update fb.sh

--- etc/fb.sh
+++ etc/fb.sh
@@ -3,20 +3,20 @@
 # Configure filebrowser
 if [ ! -f "/fb/data/config/filebrowser.db" ]; then
     filebrowser -d /fb/data/config/filebrowser.db config init
-    filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+    filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /fb/data/config/filebrowser.db users update ${GLOBAL_USER} --locale zh-cn
+        filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME --locale zh-cn
     fi
 else
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
     else
-        filebrowser -d /fb/data/config/filebrowser.db users add ${GLOBAL_USER} ${GLOBAL_PASSWORD} --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
     fi
-    filebrowser -d /fb/data/config/filebrowser.db users update ${GLOBAL_USER} -p ${GLOBAL_PASSWORD}
+    filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME -p $FRP_PASS
 fi
 
-filebrowser -d /fb/data/config/filebrowser.db config set -r / -b ${GLOBAL_PORTAL_PATH}/files -p 61801
+filebrowser -d /fb/data/config/filebrowser.db config set -r / -b $MYPATH/files -p 61801
 
 # Run filebrowser
 exec 2>&1
