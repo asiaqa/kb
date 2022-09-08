ce48c234fa179732d16a2e05b10cb8d23594c9b1
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-08 11:24:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 #tor &
+/ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log&
+if [[ $TUNNEL_TOKEN ]]
+then
+  /cf/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol auto&
+fi
 ssh-keygen -A
 cat /etc/caddy/Caddyfile | sed -e "s/\$FRP_NAME/$FRP_NAME/g" -e "s/\$MYPATH/$MYPATH/g" -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$FRP_PASS-HASH/$(caddy hash-password --plaintext $FRP_PASS)/g" >/etc/caddy/Caddyfile
 cp /x.json /temp.json
@@ -10,7 +15,6 @@ cp /frpc.ini /frp/frpc.ini
 cat /temp.json | sed -e "s/\$SSS/$SSS/g" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYPATH/$MYPATH/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/x.json 
 #rm /temp.json
 /x -config /x.json &
-/ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log &
 /fb/fb.sh&
 if [ $FRP_S == 1 ]
 then
@@ -21,10 +25,6 @@ then
   exec /usr/sbin/sshd -D -e "$@"&
   echo mp:$MYPATH | chpasswd&
 fi
-if [[ $TUNNEL_TOKEN ]]
-then
-  /cf/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol auto&
-fi
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile&
-sleep 2m
+sleep 10
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
