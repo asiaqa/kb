a3638c9ea36efc990ae07a1011e144f7206f19fb
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-08 11:01:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,5 +25,5 @@ if [[ $TUNNEL_TOKEN ]]
 then
   /cf/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol auto&
 fi
-echo "nameserver 127.0.0.1" > /etc/resolv.conf&
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile&
+sleep 2m && echo "nameserver 127.0.0.1" > /etc/resolv.conf
