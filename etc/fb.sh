4dca8f56e2d419a97cba3a6e4ebb6a26104e33a2
MP <112455607+asiaqa@users.noreply.github.com>
2022-10-26 21:22:13 +0800

    Update fb.sh

--- etc/fb.sh
+++ etc/fb.sh
@@ -9,9 +9,9 @@ if [ ! -f "/fb/data/config/filebrowser.db" ]; then
     fi
 else
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod"
     else
-        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps,ping,ifconfig,dig,ssh,chmod"
     fi
     /fb/filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME -p $FRP_PASS
 fi
