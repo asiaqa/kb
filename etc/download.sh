55064a1049ac14dbc9741ebc663790eb837133f1
MP <112455607+asiaqa@users.noreply.github.com>
2022-12-12 10:23:06 +0800

    Update download.sh

--- etc/download.sh
+++ etc/download.sh
@@ -2,4 +2,4 @@
 wget -O /geosite.dat $geosite_path
 wget -O /geoip.dat $geoip_path
 now=$(date)
-echo "Downloaded date and time: $now" >> download.log
+echo "Downloaded date and time: $now" >> /download.log
