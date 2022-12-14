f600e73a06de16d568b2c261d5f72bf34fc8d0d5
MP <112455607+asiaqa@users.noreply.github.com>
2022-12-14 23:39:56 +0800

    Update fb.sh

--- etc/fb.sh
+++ etc/fb.sh
@@ -3,15 +3,15 @@
 # Configure filebrowser
 if [ ! -f "/fb/data/config/filebrowser.db" ]; then
     /fb/filebrowser -d /fb/data/config/filebrowser.db config init
-    /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod,cd,ls"
+    /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="cd,cp,rm,crontab,sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod,cd,ls"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
         /fb/filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME --locale zh-cn
     fi
 else
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --locale zh-cn --commands="cd,cp,rm,crontab,sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod,cd,ls"
     else
-        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="sv,du,df,free,nslookup,netstat,top,ps,ping,ifconfig,dig,ssh,chmod"
+        /fb/filebrowser -d /fb/data/config/filebrowser.db users add $FRP_NAME $FRP_PASS --perm.admin --commands="cd,cp,rm,crontab,sv,du,df,free,nslookup,netstat,top,ps,ifconfig,dig,ping,ssh,chmod,cd,ls"
     fi
     /fb/filebrowser -d /fb/data/config/filebrowser.db users update $FRP_NAME -p $FRP_PASS
 fi
