ec7657d59721a3fd7bbd9125da9326cf9dd3270a
MP <112455607+asiaqa@users.noreply.github.com>
2022-12-15 13:42:29 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -39,5 +39,4 @@ caddy run --config /etc/caddy/Caddyfile --adapter caddyfile&
 #/usr/sbin/crond -f -l 8&
 #/usr/bin/crontab 21 8 * * * /download.sh
 echo '21 8,20 * * * /download.sh >> /crontab.log' >> /etc/crontabs/root&
-./download.sh&
 crond -f -l 8
