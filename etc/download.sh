e7153b7a3e814d8bfe9994883cf97e7048f00b53
MP <112455607+asiaqa@users.noreply.github.com>
2023-01-18 09:09:41 +0800

    Update download.sh

--- etc/download.sh
+++ etc/download.sh
@@ -4,4 +4,4 @@ wget -O /geosite.dat $1 && \
 wget -O /geoip.dat $geoip_path && \
 now=$(date) && \
 echo "Downloaded date and time: $now" >> /download.log && \
-/x -config /x.json >> /proc/1/fd/1 & &
+/x -config /x.json >> /proc/1/fd/1 & 
